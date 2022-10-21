5dc1b2585d8173f34eaa232ffa8a2ae60fea8d67
Colton Christopher <2tontide@gmail.com>
2022-10-21 11:58:14 -0500

    completed

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,34 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, what is your favorite school subject?");
+
+            var subject = Console.ReadLine();
+
+            switch (subject)
+            {
+                case "Math":
+                    Console.WriteLine("You must understand number");
+                    break;
+
+                case "Science":
+                    Console.WriteLine("You like to do experiments");
+                    break;
+
+                case "English":
+                    Console.WriteLine("You like to exspress yourself in your writing");
+                    break;
+
+                case "History":
+                    Console.WriteLine("You want to know the difference of today and the past");
+                    break;
+
+                default:
+                    Console.WriteLine($"Oh wow! I haven't taken that subject before. {subject} sounds fun!");
+                    break;
+
+            }
+
         }
     }
 }
